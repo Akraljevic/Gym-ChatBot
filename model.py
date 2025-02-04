67975cfcf1f0e80585769e72660d3a40ce20177f
Akraljevic <akralj23@gmail.com>
2025-02-04 23:32:59 +0100

    Poboljsan model

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -22,3 +23,45 @@ class NeuralNet(nn.Module):
 
         out = self.l3(out)
         return out
+=======
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        
+        self.l1 = nn.Linear(input_size, hidden_size * 2)
+        self.bn1 = nn.BatchNorm1d(hidden_size * 2)
+        self.dropout1 = nn.Dropout(0.3)  
+
+        self.l2 = nn.Linear(hidden_size * 2, hidden_size)
+        self.bn2 = nn.BatchNorm1d(hidden_size)
+        self.dropout2 = nn.Dropout(0.3)  
+        
+        self.l3 = nn.Linear(hidden_size, hidden_size // 2)  
+        self.bn3 = nn.BatchNorm1d(hidden_size // 2)
+        self.dropout3 = nn.Dropout(0.3)  
+
+        self.l4 = nn.Linear(hidden_size // 2, num_classes)  
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.bn1(out)
+        out = F.leaky_relu(out, negative_slope=0.01) 
+        out = self.dropout1(out)  
+
+        out = self.l2(out)
+        out = self.bn2(out)
+        out = F.leaky_relu(out, negative_slope=0.01) 
+        out = self.dropout2(out)  
+
+        out = self.l3(out)
+        out = self.bn3(out)
+        out = F.leaky_relu(out, negative_slope=0.01) 
+        out = self.dropout3(out)  
+
+        out = self.l4(out)
+        return out
+>>>>>>> b03e1fc (Poboljsan model)
--- nltk_utilites.py
+++ nltk_utilites.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import nltk
 import numpy as np
 from nltk.stem.porter import PorterStemmer
@@ -19,3 +20,25 @@ def bag_of_words(tokenized_sentence, all_words):
             bag[index] = 1.0
         
     return bag
+=======
+import nltk
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence) 
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for index, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[index] = 1.0
+        
+    return bag
+>>>>>>> b03e1fc (Poboljsan model)
