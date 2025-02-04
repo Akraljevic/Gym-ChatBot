4ae93f2f417bdacfbb36117de40bcc1cd7db1361
Akraljevic <akralj23@gmail.com>
2025-02-04 23:24:36 +0100

    Poboljsan model

--- model.py
+++ model.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size * 2)
-        self.bn1 = nn.BatchNorm1d(hidden_size * 2)
-        self.l2 = nn.Linear(hidden_size * 2, hidden_size)
-        self.bn2 = nn.BatchNorm1d(hidden_size)
-        self.l3 = nn.Linear(hidden_size, num_classes)
-        
-    def forward(self, x):
-        out = self.l1(x)
-        out = self.bn1(out)  # Batch normalization
-        out = F.relu(out)
-        
-        out = self.l2(out)
-        out = self.bn2(out)  # Batch normalization
-        out = F.relu(out)
-
-        out = self.l3(out)
-        return out
-=======
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -64,4 +38,3 @@ class NeuralNet(nn.Module):
 
         out = self.l4(out)
         return out
->>>>>>> b03e1fc (Poboljsan model)
